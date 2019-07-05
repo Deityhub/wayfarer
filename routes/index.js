34d069338f1cb73f83eb7ce822088d1919c1ec82
Ojini Chizoba Jude <judoc96@gmail.com>
2019-07-05 02:43:19 +0100

    feature(routing):route config integration [Finishes #167081071]

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const user = require('./user.route');
+const bus = require('./bus.route');
 
 module.exports = (app) => {
   app.use('/api/v1', user);
+  app.use('/api/v1', bus);
   app.use('/api/v1', (req, res) => {
     // display documentation here
     res.send('Welcome to WayFarer api...');
