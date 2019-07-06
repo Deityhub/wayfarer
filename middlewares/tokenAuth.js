ce279a9bde9b66b02802092f2cf4fae3ca30fd98
Ojini Chizoba Jude <judoc96@gmail.com>
2019-07-06 16:20:56 +0100

    feature(implementation):implement get trips [Finishes #166985123]

--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -7,10 +7,11 @@ module.exports = (req, res, next) => {
 
   if (isEmpty(authorization) || isEmpty(authorization.split(' ')[1])) {
     req.status = 401;
-    next(new Error('Unauthorized, you need to sign in'));
+    return next(new Error('Unauthorized, you need to sign in'));
   }
 
   const token = authorization.split(' ')[1];
+  // eslint-disable-next-line consistent-return
   jwt.verify(token, config.get('jwtKey'), (err, decoded) => {
     if (err) {
       req.status = 403;
@@ -18,6 +19,7 @@ module.exports = (req, res, next) => {
     }
 
     req.is_admin = decoded.is_admin;
-    return next();
   });
+
+  return next();
 };
